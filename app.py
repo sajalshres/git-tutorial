541e6658c06217cada0838111000028c169a1a4f
sajal <sajal.shres@gmail.com>
2024-04-23 23:54:55 -0400

    added third number to sum

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
-def sum(num1, num2):
-    result = num1 + num2
+def sum(num1, num2, num3):
+    result = num1 + num2 + num3
     return result
 
-result = sum(10, 20)
+result = sum(10, 20, 30)
 print("Sum is", result)
