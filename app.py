928e08ac2434d3b0e9d29fe392b1a738a4a770a9
Raunak Rajbhandari <rrajbhan213@gmail.com>
2024-04-25 21:58:23 -0400

    Adding a function

--- app.py
+++ app.py
@@ -3,5 +3,10 @@ def sum(num1, num2, num3, num4):
     result = num1 + num2 + num3 + num4
     return result
 
+ def multiply(num1, num2, num3, num4):
+    result = num1 * num2 * num3 * num4
+    return result  
+     
+
 result = sum(10, 20, 30, 40)
 print("Sum is", result)
